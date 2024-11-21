87a856d9b277f7f448ec3f84b96bd36ef4f299ea
Bruno Leal <brunopazet@gmail.com>
2024-11-21 16:18:22 -0300

    meus primeiros passos em go

---
+++ up-fast/home.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return {"message": "Item created successfully", "item": item}
